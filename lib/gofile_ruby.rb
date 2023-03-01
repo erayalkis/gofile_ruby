fecbc49b60a5bd24fcd188f2e2c80d667a806ecb
Eray Alkış <lolpo12345@outlook.com>
2023-03-01 05:55:02 +0300

    Finish create folder method

--- lib/gofile_ruby.rb
+++ lib/gofile_ruby.rb
@@ -1,12 +1,59 @@
 require './lib/helpers/http/http.rb'
 
 class GFClient
-  def self.test
-    puts "This is a test message!"
+
+  def initialize(token:nil, guest:false)
+    @token = token
+    @isGuest = guest
+    @accDetails = nil
+
+    @isGuest = false if @token
+    test_token_validity unless @isGuest
   end
 
   def get_server(parse:false)
-    target_url = "https://api.gofile.io/getServer"
-    HTTPHelper.get(target_url, parse:parse)
+    server_url = "https://api.gofile.io/getServer"
+    HTTPHelper.get(server_url, parse)
+  end
+
+  def create_folder(parent_id:nil, folder_name:, parsed:false)
+    raise "Guest accounts cannot create folders! Did you mean to upload a file instead?" if @isGuest
+    post_folder_url = "https://api.gofile.io/createFolder"
+    
+    parent_id = @accDetails["data"]["rootFolder"] unless parent_id
+    folder_data = {
+      "parentFolderId" => parent_id,
+      "folderName" => folder_name,
+      "token" => @token
+    }
+
+    ret = HTTPHelper.put(post_folder_url, folder_data, parsed)
+    
+    puts ret
+    ret
+  end
+
+  private
+
+  # Will return account details
+  # Example:
+  # {
+  #   "status": "ok",
+  #   "data": {
+  #     "token": "ivlW1ZSGn2Y4AoADbCHUjllj2cO9m3WM",
+  #     "email": "email@domain.tld",
+  #     "tier": "standard",
+  #     "rootFolder": "2aecea58-84e6-420d-b2b9-68b4add8418d",
+  #     "foldersCount": 4,
+  #     "filesCount": 20,
+  #     "totalSize": 67653500,
+  #     "totalDownloadCount": 1
+  #   }
+  # }
+  def test_token_validity
+    account_details_url = "https://api.gofile.io/getAccountDetails?token=#{@token}"
+    details = HTTPHelper.get(account_details_url, true)   
+    
+    @accDetails = details
   end
 end
--- lib/helpers/http/http.rb
+++ lib/helpers/http/http.rb
@@ -3,11 +3,41 @@ require 'net/http'
 require 'json'
 
 class HTTPHelper
-
-  def self.get(url, parse:false)
+  def self.get(url, parse)
     url = URI(url) unless url.class == URI::Generic
     res = Net::HTTP.get_response(url)
     
+    puts res.code
+    ret = res.body
+    ret = JSON.parse(ret) if parse
+
+    ret
+  end
+
+  def self.post_form(url, data, parse)
+    raise "No form data provided!" unless data
+    url = URI(url) unless url.class == URI::Generic
+
+    res = Net::HTTP.post_form(url, data)
+    
+    puts res.code
+    ret = res.body
+    ret = JSON.parse(ret) if parse
+
+    ret
+  end
+
+  def self.put(url, data, parse)
+    raise "No form data provided!" unless data
+    url = URI(url) unless url.class == URI::Generic
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    req = Net::HTTP::Put.new(url, { 'Content-Type' => 'application/x-www-form-urlencoded' })
+    req.body = URI::encode_www_form(data)
+
+    res = http.request(req)
+
     ret = res.body
     ret = JSON.parse(ret) if parse
 
