9f0d1b7a30b67ba51706e1e1174aa7245484bac9
Eray Alkış <lolpo12345@outlook.com>
2023-03-01 17:39:27 +0300

    Finish #set_folder_option method

--- lib/gofile_ruby.rb
+++ lib/gofile_ruby.rb
@@ -11,12 +11,12 @@ class GFClient
     test_token_validity unless @isGuest
   end
 
-  def get_server(parse:false)
+  def get_server
     server_url = "https://api.gofile.io/getServer"
     HTTPHelper.get(server_url, parse)
   end
 
-  def create_folder(parent_id:nil, folder_name:, parsed:false)
+  def create_folder(parent_id:nil, folder_name:)
     raise "Guest accounts cannot create folders! Did you mean to upload a file instead?" if @isGuest
     post_folder_url = "https://api.gofile.io/createFolder"
     
@@ -27,9 +27,43 @@ class GFClient
       "token" => @token
     }
 
-    ret = HTTPHelper.put(post_folder_url, folder_data, parsed)
+    ret = HTTPHelper.put(post_folder_url, folder_data)
     
     puts ret
+    ret
+  end
+
+  # Gets the children of a specific folder
+  # Defaults to root folder if parent is not provided
+  def get_children(parent:nil)
+    raise "Guests cannot use the #get_children method!" if @isGuest
+    parent = @accDetails["data"]["rootFolder"] if !parent
+
+    content_url = "https://api.gofile.io/getContent?contentId=#{parent}&token=#{@token}"
+    ret = HTTPHelper.get(content_url)
+
+    ret
+  end
+
+  # Sets the options for a specific folder
+  # Takes an option string and a matching value:
+  # public: Boolean
+  # password: String
+  # description: String
+  # expire: Unix Timestamp
+  # tags: String (String of comma separated tags, Eg. "tag1,tag2,tag3")
+  def set_folder_option(folder_id:, option:, value:)
+    options_url = "https://api.gofile.io/setFolderOption"
+
+    body = {
+      "option": option,
+      "value": value,
+      "folderId": folder_id,
+      "token": @token
+    }
+    ret = HTTPHelper.put(options_url, body)
+    puts ret 
+
     ret
   end
 
@@ -52,7 +86,7 @@ class GFClient
   # }
   def test_token_validity
     account_details_url = "https://api.gofile.io/getAccountDetails?token=#{@token}"
-    details = HTTPHelper.get(account_details_url, true)   
+    details = HTTPHelper.get(account_details_url)   
     
     @accDetails = details
   end
